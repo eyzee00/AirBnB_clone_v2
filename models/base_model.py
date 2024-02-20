0c09ca0218c5527a75393f5d9ffcca0cbbebee6f
eyzee00 <azizihamza1910@gmail.com>
2024-02-20 04:09:58 +0100

    !

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime, null
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
@@ -15,9 +23,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "__class__":
-                    continue
-                else:
+                if key != "__class__":
                     setattr(self, key, value)
 
     def __str__(self):
@@ -28,7 +34,13 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """Deletes the current instance from storage"""
+        from models import storage
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -36,4 +48,7 @@ class BaseModel:
         target_dict['created_at'] = self.created_at.isoformat()
         target_dict['updated_at'] = self.updated_at.isoformat()
         target_dict['__class__'] = self.__class__.__name__
+        key = '_sa_instance_state'
+        if key in target_dict.keys():
+            del target_dict[key]
         return target_dict
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage
+from models.city import City
+from os import getenv
+from sqlalchemy import Column, String 
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Gets a list of all related City objects"""
+            city_list = []
+            city_dictionary = storage.all(City)
+            for city in list(city_dictionary.values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
