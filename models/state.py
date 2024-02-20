6707aa927e1f8db7bd44aaa5885bf8e02a4a3053
eyzee00 <azizihamza1910@gmail.com>
2024-02-20 04:24:03 +0100

    Finished half task 6

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage
-from models.city import City
 from os import getenv
 from sqlalchemy import Column, String 
 from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
