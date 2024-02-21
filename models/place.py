a0c805f8b0df39e1dac3d7cfcd232803ab724508
y4h14 <yahyasadig2013@gmail.com>
2024-02-22 01:15:02 +0300

    updated task 10

--- models/place.py
+++ models/place.py
@@ -21,7 +21,7 @@ class Place (BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128))
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
@@ -29,6 +29,8 @@ class Place (BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship('Review', backref='place', cascade='delete')
+    amenities = relationship('Amenity', secondary=place_amenity,
+                            viewonly=False, back_populates="place_amenities")
     amenity_ids = []
 
     if os.getenv("HBNB_TYPE_STORAGE", None) != 'db':
