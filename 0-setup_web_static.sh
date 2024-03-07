934a58f1b2d7c9b0f81156501b255b26c01135be
Y4h14 <yahyasadig2013@gmail.com>
2024-03-07 22:09:51 +0300

    added task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# sets up a webstatic
+
+
+sudo apt update
+sudo apt install -y nginx
+
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<html><head></head><body>Holberton School</body></html>" > /data/releases/test/index.html
+
+ln -sf /data/web_static/current /data/web_static/releases/test/
+
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+printf %s "server {
+        listen  80 default_server;
+        listen  [::]:80 default_server;
+        root    /var/www/html;
+        index   index.html;
+
+        location /redirect_me {
+        return 301 http://googl.com;
+        }
+        error_page 404 /404.html;
+        location /404 {
+        root /var/www/html;
+        internal;
+        }
+}" > /etc/nginx/sites-available/default
+
+sudo service nginx restart
