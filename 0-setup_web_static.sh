77581dab92053861da58e97bd51708b1ec6e5aef
eyzee00 <azizihamza1910@gmail.com>
2024-03-07 22:57:51 +0100

    f

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,38 @@
 #!/usr/bin/env bash
-# sets up a webstatic
+# Sets up a web server for deployment of web_static
 
+apt-get update
+apt-get install -y nginx
 
-sudo apt update
-sudo apt install -y nginx
-
-
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
-
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hello World!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 printf %s "server {
-        listen  80 default_server;
-        listen  [::]:80 default_server;
-        root    /var/www/html;
-        index   index.html;
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-	location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
-	}
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-        location /redirect_me {
-        return 301 http://googl.com;
-        }
-        error_page 404 /404.html;
-        location /404 {
-        root /var/www/html;
-        internal;
-        }
+    location /redirect_me {
+        return 301 http://youtube.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 }" > /etc/nginx/sites-available/default
 
-sudo service nginx restart
+service nginx restart
