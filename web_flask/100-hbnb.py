4ce91311b84aaa24caa12d17ec2342d9699682fb
eyzee00 <azizihamza1910@gmail.com>
2024-03-25 02:27:54 +0000

    f

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -8,13 +8,14 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
     """Displays the main HBnB filters HTML page."""
     states = storage.all("State")
     amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+    places = storage.all("Place")
+    return render_template("100-hbnb.html",
+                           states=states, amenities=amenities, places=places)
 
 
 @app.teardown_appcontext
